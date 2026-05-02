6fad84a24331f6e18f1f19fb60831ac331472a6b
nightwatcher23 <maren@rautmann.eu>
2026-05-02 11:35:56 +0200

    catchBtn works, few touch-ups still necessary

--- 06/Bonus 06/catchBtn.js
+++ 06/Bonus 06/catchBtn.js
@@ -9,8 +9,9 @@ let y = 0;
 let targetX = 0;
 let targetY = 0;
 
-const speed = 0.001;
-const distance;
+let speed = 0.001;
+let distance = 0;
+const maxDistance = 1000;
 
 let didMouseTouch = false;
 
@@ -19,24 +20,17 @@ let stopTimeout;
 
 //saves mouse position + calculates distance between btn and mouse
 document.addEventListener('mousemove', (e) => {
+    console.log('stop moving')
     isMouseMoving = true;
 
     mouseX = e.clientX;
     mouseY = e.clientY;
 
-    const rect = btn.getBoundingClientRect();
-    const btnMiddleX = rect.left + rect.width / 2;
-    const btnMiddleY = rect.top + rect.height / 2;
-
-    const dX = mouseX - btnMiddleX;
-    const dY = mouseY - btnMiddleY;
-
-    const distance = Math.hypot(dX, dY);
-
     clearTimeout(stopTimeout);
     //if mouse has stopped moving for a certain time, saves target position for movement
     stopTimeout = setTimeout(() => {
         if (!didMouseTouch) {
+            console.log('coming closer')
             isMouseMoving = false;
 
             targetX = mouseX;
@@ -48,16 +42,34 @@ document.addEventListener('mousemove', (e) => {
 })
 
 //sends signal if mouse touches btn + sets target position to a random position on screen
-btn.addEventListener('mouseover', () => {
+//within a certain distance of the element
+btn.addEventListener('mouseover', function() {
+    flyYouFool();
+}) 
+
+function flyYouFool () {
+    console.log('it TOUCHED');
     const maxX = window.innerWidth - btn.offsetWidth;
     const maxY = window.innerHeight - btn.offsetHeight;
 
     didMouseTouch = true;
-    speed = 0.5;
 
     targetX = Math.random() * maxX;
     targetY = Math.random() * maxY;
-})
+
+    speed = 0.5;
+
+    const rect = btn.getBoundingClientRect();
+    const btnCenterX = rect.left + rect.width / 2;
+    const btnCenterY = rect.top + rect.height / 2;
+
+    const dX = btnCenterX - mouseX;
+    const dY = btnCenterY - mouseY;
+
+    distance = Math.sqrt(dX * dX + dY * dY);
+
+    return distance;
+}
 
 //moves btn towards target position if mouse has stopped moving or has touched btn
 function animate () {
@@ -65,11 +77,15 @@ function animate () {
         x += (targetX - x) * speed;
         y += (targetY - y) * speed;
 
-        //PROBLEM! has to be checked again 
-        // -> Goal: resets mouse touch, so that element might move closer again
-        if(distance > 10) {
+        if( distance <= maxDistance) {
             didMouseTouch = false;
         }
+        
+    } else {
+        x += 0;
+        y += 0;
+        
+        didMouseTouch = false;
     }
 
     btn.style.transform = `translate(${x}px, ${y}px)`
