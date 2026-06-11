b231920cc9da837f801c6ff375d735da5d774731
nightwatcher23 <maren@rautmann.eu>
2026-06-11 16:41:05 +0200

    Javascript update

--- 07_404/404script.js
+++ 07_404/404script.js
@@ -1,5 +1,11 @@
-errorBtn.addEventListener('mouseover', function(){
+const errorBtn = document.getElementsByClassName('container yellow')
+
+errorBtn.addEventListener('click', function() {
+    changeColor()
+});
+
+function changeColor() {
     console.log("hit");
     errorBtn.classList.remove('yellow');
     errorBtn.classList.add('green');
-})
+}
